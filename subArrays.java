ea26dfe8bcf8737f984168362550dcd08a98d079
Vikash Patel <vpatel884@gmail.com>
2023-02-11 17:49:37 +0530

    feat : to count total numbers of sub array

--- subArrays.java
+++ subArrays.java
@@ -1,16 +1,19 @@
 public class subArrays {
 
     public static void subArray(int arr[]){
-        
+        int totalSubArray = 0;
         for(int i=0; i<arr.length; i++){
             for(int j=i; j<arr.length; j++){
                 for(int k=i; k<=j; k++){
                     System.out.print(arr[k]+" ");
+                    
                 }
+                totalSubArray++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total numbers of sub arrays are : "+totalSubArray);
     }
 
     public static void main(String args[]){
